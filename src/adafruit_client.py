0552ebc264a021903bdaec7f6936a67fc98c72e9
Maria Rita Lopes Silva <mr.lopessil@gmail.com>
2026-07-01 14:50:08 -0300

    feat(mqtt): add control functions for Adafruit IO feeds (lights, ac, alarm, last-person, intruder)

---
+++ src/adafruit_client.py
@@ -0,0 +1,30 @@
+from Adafruit_IO import MQTTClient
+from config import AIO_USERNAME, AIO_KEY, FEED_LIGHT, FEED_AC, FEED_INTRUDER, FEED_LAST_PERSON, FEED_ALARM
+
+client = MQTTClient(AIO_USERNAME, AIO_KEY)
+client.connect()
+client.loop_background()
+
+def last_seen(stream):
+    client.publish(FEED_LAST_PERSON, stream)
+
+def invader(stream):
+    client.publish(FEED_INTRUDER, stream)
+
+def alarm_on():
+    client.publish(FEED_ALARM, "ON")
+
+def ac_on():
+    client.publish(FEED_AC, "ON")
+
+def light_on():
+    client.publish(FEED_LIGHT, "ON")
+
+def alarm_off():
+    client.publish(FEED_ALARM, "OFF")
+
+def ac_off():
+    client.publish(FEED_AC, "OFF")
+
+def light_off():
+    client.publish(FEED_LIGHT, "OFF")
--- src/esp32.py
+++
@@ -1,6 +0,0 @@
-import serial
-from config import SERIAL_PORT, BAUD_RATE
-
-class ESP32Controller:
-    def __init__(self):
-        self.serial = serial.Serial(SERIAL_PORT, BAUD_RATE)
